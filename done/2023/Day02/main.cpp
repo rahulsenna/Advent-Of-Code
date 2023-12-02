bcf10d26d7cade70c5179d1f02d6ced99f0cacf3
Rahul Senna <rahulvsenna@gmail.com>
2023-12-03 04:56:15 +0530

    removed unnecessary code

--- done/2023/Day02/main.cpp
+++ done/2023/Day02/main.cpp
@@ -13,23 +13,18 @@ int main()
     {
         stringstream ss(line);
         ss >> label >> id >> _;
-        vector<pair<string,int>> cubes; // part1
-        unordered_map<string, int> max_cubes = {{"red",1},{"blue",1},{"green",1}}; // part2
+        unordered_map<string, int> max_cubes = {{"red",1},{"blue",1},{"green",1}};
         while(ss)
         {
             ss >> count >> color;
             if (color.back() == ',' || color.back() == ';')
                 color.pop_back();
-
-            cubes.push_back({color, count});       // part1
-            if (max_cubes[color] < count)          // part2
+            if (max_cubes[color] < count)
             	max_cubes[color] = count;   
         }
         res2 += accumulate(max_cubes.begin(), max_cubes.end(), 1, [](int acc,auto &c) { return acc*c.second;});
-        sort(cubes.begin(), cubes.end(), [](const auto &l, const auto &r) { return l.second > r.second; });
-        bool is_possible = (cubes.front().second < 13) || (cubes.front().second == 13 && cubes.front().first != "red") || (cubes.front().second == 14 && cubes.front().first == "blue");
-        if (is_possible)
-            res1 += id;                
+        if ((max_cubes["red"] < 13) && (max_cubes["green"] < 14) && (max_cubes["blue"] < 15))
+            res1 += id;      
     }
     
     printf("part_1: %d\n", res1);
